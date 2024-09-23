91c8fa359f70930f94ae9da21bc8b153a0bd236b
Your Name <you@example.com>
2024-09-23 15:31:32 +0300

    added file for drop all tables in correct order

---
+++ drop_all_tables.sql
@@ -0,0 +1,14 @@
+use botransactions
+drop table contracts;
+drop table cardholders;
+drop table persons;
+drop table addresses;
+drop table customers;
+drop table accounts;                 
+drop table acquirers;                
+drop table card_instances;           
+drop table cards;                   
+drop table flexible_data;           
+drop table issuers;                 
+drop table transactions;   
+drop table operations;    
