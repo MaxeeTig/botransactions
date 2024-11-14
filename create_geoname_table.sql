569a1abaae0b1a865875394ad9f14f48f00429ab
Your Name <you@example.com>
2024-11-14 08:16:13 +0300

    imp: added geoname cities table

--- create_geoname_table.sql
+++ create_geoname_table.sql
@@ -1,7 +1,9 @@
- CREATE TABLE geoname (
+use botransactions;
+CREATE TABLE geoname (
     geonameid INT,
     name VARCHAR(200) CHARACTER SET utf8,
     asciiname VARCHAR(200),
     alternatenames VARCHAR(10000),
     country_code CHAR(2)
-) ENGINE=InnoDB;
+) 
+
